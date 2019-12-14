c32df43ff5d187c540ff21b0154ee815f64c8b70
Christophe Leray <christophe.leray1@gmail.com>
2019-12-14 10:57:32 +0100

    fix issue with trix and add rich_text descption in hotel model

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -3,7 +3,7 @@ class HotelsController < InheritedResources::Base
   private
 
     def hotel_params
-      params.require(:hotel).permit(:name, :phone_number, :city, :country_code, :postal_code, :state, :street_name)
+      params.require(:hotel).permit(:name, :phone_number, :city, :country_code, :postal_code, :state, :street_name, :description)
     end
 
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,3 @@
 class Hotel < ApplicationRecord
+    has_rich_text :description
 end
