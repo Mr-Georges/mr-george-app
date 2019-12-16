1c41639f3c0d77228e4890db3ad66beff5e92d8d
Christophe Leray <christophe.leray1@gmail.com>
2019-12-16 14:21:08 +0100

    add enum status to hotel model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,12 @@
 class Hotel < ApplicationRecord
+    #before_validation :set_hotel_status
+
     has_rich_text :description
+
+    enum status: {
+        published: "published", draft: "draft", pending: "pending", canceled: "canceled"
+    }, _prefix: :status
+
+    validates :name, presence: true
+    #validates :status, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_155118) do
+ActiveRecord::Schema.define(version: 2019_12_16_125608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,17 +72,8 @@ ActiveRecord::Schema.define(version: 2019_12_13_155118) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "hotels", force: :cascade do |t|
-    t.string "name"
-    t.string "phone_number"
-    t.string "city"
-    t.string "country_code"
-    t.string "postal_code"
-    t.string "state"
-    t.string "street_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+# Could not dump table "hotels" because of following StandardError
+#   Unknown type 'hotel_status' for column 'status'
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
