6b4adb5985ca3f81433559faf212f7e3fd574972
Christophe Leray <christophe.leray1@gmail.com>
2020-01-12 20:41:46 +0100

    update before_action typo

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
-    before_action: set_activity, only: %i[show edit update destroy]
+    before_action :set_activity, only: %i[show edit update destroy]
 
 
     def index
